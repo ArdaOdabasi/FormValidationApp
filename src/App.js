733d24669800e159fa11c05550fd8512b39d85fd
Arda Odabasi <ardaodabasi@hotmail.com>
2023-10-09 22:15:40 +0300

    Validation processes were carried out in the App.js file. The project has been completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { userValidation } from './Validation';
 
 function App() {
+  const createUser = async (event) => {
+    event.preventDefault();
+    let formData = {
+      name: event.target[0].value,
+      email: event.target[1].value,
+      password: event.target[2].value,
+    };
+    console.log(formData)
+
+    const isValid = await userValidation.isValid(formData);
+    if (isValid) {
+      console.log("Bilgiler geçerli. Gönderme işlemi başarılı!");
+    } else {
+      console.log("Bilgiler geçerli değil. Lütfen gerekli alanları doldurun ve kurallara uyun!");
+    }
+  }
+
+  // function createUser(event) {
+  //   event.preventDefault();
+  //   let formData = {
+  //     name: event.target[0].value,
+  //     email: event.target[1].value,
+  //     password: event.target[2].value,
+  //   };
+  //   console.log(formData);
+  // }
+
   return (
     <div className="App">
       <p>Form Validation Application</p>
-      <form className='form'>
+      <form className='form' onSubmit={createUser}>
         <input type="text" placeholder="Adınızı Girin"></input>
         <input type="email" placeholder="Mailinizi Girin"></input>
         <input type="password" placeholder="Şifrenizi Girin"></input>
