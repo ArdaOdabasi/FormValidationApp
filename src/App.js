55a5a5c31649d35a3130c63c50d918c46cf36072
Arda Odabasi <ardaodabasi@hotmail.com>
2023-10-09 20:34:46 +0300

    Form and form elements were created.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <form className='form'>
+        <input type="text" placeholder="Adınızı Girin.."></input>
+        <input type="email" placeholder="Mailinizi Girin.."></input>
+        <input type="password" placeholder="Şifrenizi Girin.."></input>
+        <input type="submit" className="submit"></input>
+      </form>
     </div>
   );
 }
