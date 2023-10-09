f9cc2e17fcfc65dea00d3fc77c3e8c1734a04bcc
Arda Odabasi <ardaodabasi@hotmail.com>
2023-10-09 21:00:55 +0300

    CSS codes were written.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>Form Validation Application</p>
       <form className='form'>
-        <input type="text" placeholder="Adınızı Girin.."></input>
-        <input type="email" placeholder="Mailinizi Girin.."></input>
-        <input type="password" placeholder="Şifrenizi Girin.."></input>
+        <input type="text" placeholder="Adınızı Girin"></input>
+        <input type="email" placeholder="Mailinizi Girin"></input>
+        <input type="password" placeholder="Şifrenizi Girin"></input>
         <input type="submit" className="submit"></input>
       </form>
     </div>
